12455bbcf6c1cff0c9283eeeb85cb0a2eb58eae7
Xiaoyu_SZ <z-xy18@mails.tsinghua.edu.cn>
2021-10-23 16:22:31 +0800

    update script

--- UBFC/post_process.py
+++ UBFC/post_process.py
@@ -7,16 +7,17 @@
 
 # UBFC datasets:https://drive.google.com/drive/folders/1Iu7mdh9oTbsX5YbQjHfzrFOAjn-AdnZG?usp=sharing
 
-# TODO:
-# you can config the file imformation here
 
 import os
-import matplotlib as plt
+from matplotlib import pyplot as plt
 from scipy.sparse import spdiags
 from scipy.signal import butter
 import scipy.io
 import scipy
 import numpy as np
+
+# TODO:
+# you can config the file imformation here
 SUBJECT = "subject1"
 FILE_NAME = "ground_truth.txt"
 # TODO ENDS
@@ -111,7 +112,7 @@ def calculate_SNR(pxx_pred, f_pred, currHR, signal):
 # %%  Processing
 
 
-def calculate_metric_peak_per_video(predictions, labels, signal='pulse',
+def calculate_metric_peak_per_video(predictions, labels,signal='pulse',
                                     fs=30, bpFlag=True):
     if signal == 'pulse':
         [b, a] = butter(1, [0.75 / fs * 2, 2.5 / fs * 2],
@@ -137,6 +138,7 @@ def calculate_metric_peak_per_video(predictions, labels, signal='pulse',
     labels_peaks, _ = scipy.signal.find_peaks(label_window)
     preds_peaks, _ = scipy.signal.find_peaks(pred_window)
 
+
     temp_HR_0 = 60 / (np.mean(np.diff(labels_peaks)) / fs)
     temp_HR = 60 / (np.mean(np.diff(preds_peaks)) / fs)
 
@@ -147,8 +149,7 @@ def calculate_metric_peak_per_video(predictions, labels, signal='pulse',
 
     HR = np.mean(np.array(HR_pred))
     HR0 = np.mean(np.array(HR0_pred))
-
-    return HR0, HR
+    return HR0, HR,labels_peaks,label_window
 
 
 def calculate_metric_FFT_per_video(predictions, labels, signal='pulse',
@@ -168,7 +169,7 @@ def calculate_metric_FFT_per_video(predictions, labels, signal='pulse',
     if bpFlag:
         pred_window = scipy.signal.filtfilt(b, a, np.double(pred_window))
         label_window = scipy.signal.filtfilt(b, a, np.double(label_window))
-
+    filt_signal = pred_window
     pred_window = np.expand_dims(pred_window, 0)
     label_window = np.expand_dims(label_window, 0)
     # Predictions FFT
@@ -191,54 +192,52 @@ def calculate_metric_FFT_per_video(predictions, labels, signal='pulse',
     else:
         # regular Heart beat are 0.75*60 and 2.5*60
         fmask_label = np.argwhere((f_label >= 0.08) & (f_label <= 0.5))
-    label_window = np.take(f_label, fmask_label)
-
+    label_window,pxx = np.take(f_label, fmask_label),np.take(pxx_label, fmask_label)
     # MAE
     temp_HR, temp_HR_0 = calculate_HR(
         pxx_pred, pred_window, fmask_pred, pxx_label, label_window, fmask_label)
 
-    return temp_HR_0, temp_HR
+    return temp_HR_0, temp_HR,label_window,pxx
 
 
-#
-# def plot_signal(pred_signal, label_signal, video_filename, save_path, HR0, HR,
-#                     HR0_peak, HR_peak, peaks, peaks0):
-#     '''
-#     HR0: Ground Truth Heart Rate using FFT
-#     HR: Predicted Heart Rate using nfft
-#     HR0_peak: Ground Truth Heart Rate using Peak Detection
-#     HR_peak: Predicted Heart Rate using Peak Detection
-#     peaks: Peak locations from predicted signal
-#     peaks0: Peak locations from ground-truth signal
-#     '''
-#     pred_signal = np.array(pred_signal)
-#     label_signal = np.array(label_signal)
-#     fig = plt.gcf()
-#     fig.set_size_inches(20, 12)
-#     figure_title = video_filename + ' | ' + ' | HR_FFT: ' + str(HR) + ' | HR0_FFT: ' + str(HR0) + ' | HR_peak: ' + str(HR_peak) \
-#                         + ' | HR0_peak: ' + str(HR0_peak) + ' | MAE-FFT: ' + str(abs(HR0-HR)) + ' | MAE-Peak: ' \
-#                         + str(abs(HR0_peak-HR_peak))
-#     fig.suptitle(figure_title, fontsize=15)
-#     plt.subplot(211)
-#     plt.plot(pred_signal)
-#     plt.plot(peaks, signal[peaks], "x")
-#     plt.title('Prediction')
-#     plt.show()
-#     plt.subplot(212)
-#     plt.plot(label_signal)
-#     plt.plot(peaks0, label_signal[peaks0], "x")
-#     plt.title('Label')
-#     plt.show()
-#     plt.savefig(os.path.join(save_path, video_filename + '.png'))
-#     plt.close()
+
+def plot_signal(label_signal,peaks0,HR,label_window,pxx,HR_FFT):
+    '''
+    HR0: Ground Truth Heart Rate using FFT
+    HR: Predicted Heart Rate using nfft
+    HR0_peak: Ground Truth Heart Rate using Peak Detection
+    HR_peak: Predicted Heart Rate using Peak Detection
+    peaks: Peak locations from predicted signal
+    peaks0: Peak locations from ground-truth signal
+    '''
+    plt.subplot(211)
+    plt.plot(label_window, np.reshape(pxx, [-1, 1]))
+    plt.title("FFT:"+str(HR_FFT))
+    label_signal = np.array(label_signal)
+    plt.subplot(212)
+    plt.plot(label_signal)
+    plt.plot(peaks0, label_signal[peaks0], "x")
+    plt.title("PEAK:"+str(HR))
+    plt.show()
+
+def plot_fft(signal,HR):
+    label_signal = np.array(signal)
+    plt.subplot(211)
+    plt.plot(label_signal)
+    plt.title(HR)
+    plt.show()
 
 # 先读UBFC的wave
 
-
+fig = plt.gcf()
+fig.set_size_inches(20, 12)
+# TODO: you can give the figure a name here:
+fig.suptitle("Figure Name", fontsize=15)
 bvp, hr, times,fs  = read_bvp()
 print("fs:",fs)
 print("average hr from ground truth label:", np.mean(np.array(hr)))
-HR0, HR = calculate_metric_peak_per_video(bvp, bvp)
+HR0, HR,peaks,signal = calculate_metric_peak_per_video(bvp, bvp)
 print("average hr from peak detection:", HR0)
-HR0, HR = calculate_metric_FFT_per_video(bvp, bvp)
+HR0_FFT, HR_FFT,label_window,pxx = calculate_metric_FFT_per_video(bvp, bvp)
 print("average hr from fft:", HR0)
+plot_signal(signal,peaks,HR0,label_window,pxx,HR_FFT)
