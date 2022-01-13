755d71c80993e46ee9dd195d53f23e7df5c07455
Xiaoyu_SZ <z-xy18@mails.tsinghua.edu.cn>
2022-01-13 22:33:01 +0800

    issue5-dataloader

--- dataset/data_loader/UBFC_loader.py
+++ dataset/data_loader/UBFC_loader.py
@@ -1,72 +1,87 @@
-# TODO
+"""The dataloader for UBFC datasets.
+
+Details for the UBFC-RPPG Dataset see https://sites.google.com/view/ybenezeth/ubfcrppg.
+If you use this dataset, please cite this paper:
+S. Bobbia, R. Macwan, Y. Benezeth, A. Mansouri, J. Dubois, "Unsupervised skin tissue segmentation for remote photoplethysmography", Pattern Recognition Letters, 2017.
+"""
 import os
-from dataset.data_loader.data_loader import data_loader
-from dataset.preprocess import data_preprocess
-import numpy as np
 import cv2
+import numpy as np
+from dataset.data_loader.data_loader import data_loader
 
 
 class UBFC_loader(data_loader):
-    '''data loader for UBFC dataset'''
+    """The data loader for the UBFC dataset."""
 
-    def __init__(self, video_file, bvp_file, name):
-        """initialization"""
-        super().__init__(video_file, bvp_file, name)
+    def __init__(self, data_dirs, cached_dir):
+        """Initializes an UBFC dataloader.
+            Args:
+                data_dirs(list): A list of paths storing raw video and bvp data.
+                e.g. [UBFC/subject1,UBFC/subject2,...,UBFC/subjectn] for below dataset structure:
+                -----------------
+                     UBFC/
+                     |   |-- subject1/
+                     |       |-- vid.avi
+                     |       |-- ground_truth.txt
+                     |   |-- subject2/
+                     |       |-- vid.avi
+                     |       |-- ground_truth.txt
+                     |...
+                     |   |-- subjectn/
+                     |       |-- vid.avi
+                     |       |-- ground_truth.txt
+                -----------------
+                cached_dir(str): The directory where preprocessing results are stored.
+        """
+        super().__init__(cached_dir)
+        self.data_dirs = data_dirs
+        self.preprocess()
+
 
     def __len__(self):
-        '''calculate the length of bvps'''
-        assert(self.preprocess)
-        return self.count
+        """Returns the length of the dataset."""
+        return self.len
 
     def __getitem__(self, index):
-        '''get the item of certain index'''
-        x = np.load(self.xs[index])
-        y = np.load(self.ys[index])
-        x = np.transpose(x, (3, 0, 1, 2))
-        return x, y
+        """Returns a clip of video(3,T,W,H) and it's corresponding signals(T)."""
+        input = np.load(self.inputs[index])
+        label = np.load(self.labels[index])
+        input = np.transpose(input, (3, 0, 1, 2))
+        return input, label
 
-    def preprocessing(self):
-        '''preprocessiong of the dataset'''
-        # TODO: add configure
-        self.preprocess = True
-        file_num = len(self.bvp_files)
+    def preprocess(self, w=128, h=128, clip_length=64, crop_face=True):
+        """Preprocesses the raw data."""
+        file_num = len(self.data_dirs)
         for i in range(file_num):
-            self.read_video(self.video_files[i])
-            self.read_wave(self.bvp_files[i])
-            # self.synchronize()
-            self.resize(128, 128)
-            self.chunk(64)
-            self.save()
-        print(self.name, "data preprocess done")
+            frames = self.read_video(
+                os.path.join(
+                    self.data_dirs[i],
+                    "vid.avi"))
+            bvps = self.read_wave(
+                os.path.join(
+                    self.data_dirs[i],
+                    "ground_truth.txt"))
+            frames = self.resize(frames, w, h, crop_face)
+            frames_clips, bvps_clips = self.chunk(frames, bvps, clip_length)
+            self.len += self.save(frames_clips, bvps_clips, self.data_dirs[i])
 
     def read_video(self, video_file):
-        '''read the video from UBFC dataset'''
+        """Reads a video file, returns frames(T,H,W,3) """
         VidObj = cv2.VideoCapture(video_file)
         VidObj.set(cv2.CAP_PROP_POS_MSEC, 0)
-        FrameRate = VidObj.get(cv2.CAP_PROP_FPS)
-        FN = 0
         success, frame = VidObj.read()
-        RGB = []
-
+        frames = []
         while(success):
-            # TODO: if different region
             frame = cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2RGB)
             frame = np.asarray(frame)
-
-            RGB.append(frame)
+            frames.append(frame)
             success, frame = VidObj.read()
-        self.frames = np.asarray(RGB)
+        return np.asarray(frames)
 
     def read_wave(self, bvp_file):
-        '''read the bvp signal from UBFC dataset'''
+        """Reads a bvp signal file."""
         with open(bvp_file, "r") as f:
             str1 = f.read()
             str1 = str1.split("\n")
-            bvp = str1[0]
-            bvp = [float(x) for x in bvp.split()]
-            hr = str1[1]
-            hr = [float(x) for x in hr.split()]
-            times = str1[2]
-            times = [float(x) for x in times.split()]
-            fs = len(times) / times[-1]
-        self.bvps = np.asarray(bvp)
+            bvp = [float(x) for x in str1[0].split()]
+        return np.asarray(bvp)
--- dataset/data_loader/data_loader.py
+++ dataset/data_loader/data_loader.py
@@ -1,173 +1,106 @@
-# TODO:
-import numpy as np
-from dataset.preprocess import data_preprocess
-import os
-from torch.utils.data import Dataset
-import cv2
+"""The Base Class for data-loading.
 
+Provides a pytorch-style data-loader for end-to-end training pipelines.
+Extend the class to support specific datasets.
+Dataset already supported:UBFC(TODO: add more dataset)
+"""
+import numpy as np
+import os
+import cv2
+from torch.utils.data import Dataset
 
 class data_loader(Dataset):
+    """The base class for data loading based on pytorch Dataset.
+
+    The dataloader supports both providing data for pytorch training and common data-preprocessing methods,
+    including reading files, resizing each frame, chunking, and video-signal synchronization.
+    """
     @staticmethod
     def add_data_loader_args(parser):
-        """Adds arguments to Paser for training process"""
+        """Adds arguments to parser for training process"""
         parser.add_argument(
-            "--cached_dir", default="/mnt/data0/UBFC/", type=str)
+            "--cached_dir", default="preprocessed", type=str)
+        parser.add_argument(
+            "--preprocessing", default=True, type=bool)
         return parser
-    '''data loader for UBFC dataset'''
 
-    def __init__(self, video_files, bvp_files, name):
-        '''initialization'''
-        self.video_files = video_files
-        self.bvp_files = bvp_files
-        self.xs = []
-        self.ys = []
-        self.preprocess = False
-        self.count = 0
-        self.name = name
+    def __init__(self, cached_dir):
+        """Inits dataloader with lists of files.
 
-    def read_video(self):
-        '''read the video from UBFC dataset'''
+        Args:
+            cached_dir(str): The directory where preprocessing results are stored.
+        """
+        self.cached_dir = cached_dir
+        self.inputs = []
+        self.labels = []
+        self.len = 0
+
+    def preprocess(self, w, h, clip_length, crop_face):
+        """Preprocesses the raw data.
+
+        Args:
+            w,h(int): The shape of each frame after preprocessing.
+            clip_length(int): The length of each clip.
+            crop__face(bool): Whether to crop the face area
+        """
         pass
 
-    def read_bvp(self):
-        '''read the bvp signal from UBFC dataset'''
+    def read_video(self, video_path):
+        """Reads the file, returns frames."""
         pass
 
-    def resize(self, H, W):
-        '''resize the frame for preprocessing'''
-        face_region = data_preprocess.facial_detection(self.frames[0])
-        self.frames = data_preprocess.resize(self.frames, H, W, face_region)
+    def read_bvp(self, bvp_path):
+        """Reads the file, return bvp signals."""
+        pass
 
-    def synchronize(self):
-        '''synchronize the video frames and the bvps'''
-        self.frames, self.bvps = data_preprocess.synchronize(
-            self.frames, self.bvps, True)
+    def facial_detection(self, frame):
+        """Conducts face detection on a single frame. """
+        detector = cv2.CascadeClassifier(
+            './dataset/haarcascade_frontalface_default.xml')
+        face_zone = detector.detectMultiScale(frame)
+        if (len(face_zone) < 1):
+            print("ERROR:No Face Detected")
+        if (len(face_zone) >= 2):
+            print("WARN:More than one faces are detected(Only cropping one face)")
+        return face_zone[0]
 
-    def chunk(self, clip_length):
-        '''wrap certain length of data into chunks'''
-        self.frames, self.bvps = data_preprocess.ubfc_chunk(
-            self.frames, self.bvps, clip_length)
+    def resize(self, frames, w, h, detect_face=True):
+        """Resizes each frame, crops the face area if flag is true."""
+        face_region = self.facial_detection(frames[0])
+        resize_frames = np.zeros((frames.shape[0], h, w, 3))
+        for i in range(0, frames.shape[0]):
+            frame = frames[i]
+            if detect_face:
+                frame = frame[face_region[1]:face_region[1] + face_region[3],
+                              face_region[0]:face_region[0] + face_region[2]]
+            resize_frames[i] = cv2.resize(frame, (w, h))
+        return resize_frames
 
-    def save(self):
-        '''save the preprocessed data to the given path'''
-        if (not os.path.exists("preprocessed")):
-            os.mkdir("preprocessed")
-        for i in range(len(self.bvps)):
-            assert (len(self.xs) == len(self.ys))
-            assert (len(self.xs) == (self.count))
-            x_path_name = "preprocessed" + os.sep + \
-                          str(self.name) + str(self.count) + "_x.npy"
-            y_path_name = "preprocessed" + os.sep + \
-                          str(self.name) + str(self.count) + "_y.npy"
-            self.xs.append(x_path_name)
-            self.ys.append(y_path_name)
-            np.save(x_path_name, self.frames[i])
-            np.save(y_path_name, self.bvps[i])
-            self.count += 1
+    def chunk(self, frames, bvps, clip_length):
+        """Chunks the data into clips."""
+        assert (frames.shape[0] == bvps.shape[0])
+        clip_num = frames.shape[0] // clip_length
+        frames_clips = [
+            frames[i * clip_length:(i + 1) * clip_length] for i in range(clip_num)]
+        bvps_clips = [
+            bvps[i * clip_length:(i + 1) * clip_length] for i in range(clip_num)]
+        return np.array(frames_clips), np.array(bvps_clips)
 
-
-# class VIPL_loader(data_loader):
-#     def __init__(self, video_file, bvp_file):
-#         super(VIPL_loader, self).__init__(video_file, bvp_file)
-#         self.frames = file_io.read_video(self.video_file)
-#         self.bvps = file_io.read_wave(self.bvp_file)
-#
-#     def resize(self, H, W):
-#         return data_preprocess.resize(self.frames, H, W)
-#
-#     def synchronize(self):
-#         return data_preprocess.synchronize(self.frames, self.bvps, True)
-
-#
-# class UBFC_loader(data_loader):
-#     def __init__(self, video_file, bvp_file, name):
-#         super(UBFC_loader, self).__init__(video_file, bvp_file, name)
-#
-#     def __len__(self):
-#         assert(self.preprocess)
-#         return self.count
-#
-#     def __getitem__(self, index):
-#         x = np.load(self.xs[index])
-#         y = np.load(self.ys[index])
-#         x = np.transpose(x, (3, 0, 1, 2))
-#         return x, y
-#         # return super().__getitem__(index)
-#
-#     def preprocessing(self):
-#         # TODO: add configure
-#         self.preprocess = True
-#         file_num = len(self.bvp_files)
-#         for i in range(file_num):
-#             self.read_video(self.video_files[i])
-#             self.read_wave(self.bvp_files[i])
-#             # self.synchronize()
-#             self.resize(128, 128)
-#             self.chunk(64)
-#             self.save()
-#         print(self.name, "data preprocess done")
-#
-#     def read_video(self, video_file):
-#         VidObj = cv2.VideoCapture(video_file)
-#         VidObj.set(cv2.CAP_PROP_POS_MSEC, 0)
-#         FrameRate = VidObj.get(cv2.CAP_PROP_FPS)
-#         FN = 0
-#         success, frame = VidObj.read()
-#         RGB = []
-#
-#         while(success):
-#             # TODO: if different region
-#             frame = cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2RGB)
-#             frame = np.asarray(frame)
-#
-#             RGB.append(frame)
-#             success, frame = VidObj.read()
-#         self.frames = np.asarray(RGB)
-#
-#     def read_wave(self, bvp_file):
-#         with open(bvp_file, "r") as f:
-#             str1 = f.read()
-#             str1 = str1.split("\n")
-#             bvp = str1[0]
-#             bvp = [float(x) for x in bvp.split()]
-#             hr = str1[1]
-#             hr = [float(x) for x in hr.split()]
-#             times = str1[2]
-#             times = [float(x) for x in times.split()]
-#             fs = len(times) / times[-1]
-#         self.bvps = np.asarray(bvp)
-#
-#     def resize(self, H, W):
-#         face_region = data_preprocess.facial_detection(self.frames[0])
-#         self.frames = data_preprocess.resize(self.frames, H, W, face_region)
-#
-#     def synchronize(self):
-#         self.frames, self.bvps = data_preprocess.synchronize(
-#             self.frames, self.bvps, True)
-#
-#     def chunk(self, clip_length):
-#         self.frames, self.bvps = data_preprocess.ubfc_chunk(
-#             self.frames, self.bvps, clip_length)
-#
-#     def form_h5(self, file_name):
-#         f = h5py.File(file_name, 'w')
-#         f.create_dataset('xsub', data=self.xsub)
-#         f.create_dataset('ysub', data=self.ysub)
-#         f.close()
-#
-#     def save(self):
-#         if(not os.path.exists("preprocessed")):
-#             os.mkdir("preprocessed")
-#         for i in range(len(self.bvps)):
-#             assert(len(self.xs) == len(self.ys))
-#             assert(len(self.xs) == (self.count))
-#             x_path_name = "preprocessed"+os.sep + \
-#                 str(self.name)+str(self.count)+"_x.npy"
-#             y_path_name = "preprocessed"+os.sep + \
-#                 str(self.name)+str(self.count)+"_y.npy"
-#             self.xs.append(x_path_name)
-#             self.ys.append(y_path_name)
-#             np.save(x_path_name, self.frames[i])
-#             np.save(y_path_name, self.bvps[i])
-#             self.count += 1
-#
+    def save(self, frames_clips, bvps_clips, filename):
+        """Saves the preprocessing data."""
+        if (not os.path.exists(self.cached_dir)):
+            os.mkdir(self.cached_dir)
+        count = 0
+        filename = os.path.split(filename)[-1]
+        for i in range(len(bvps_clips)):
+            assert (len(self.inputs) == len(self.labels))
+            input_path_name = self.cached_dir + os.sep + \
+                "{0}_input{1}.npy".format(filename, str(count))
+            label_path_name = self.cached_dir + os.sep + \
+                "{0}_label{1}.npy".format(filename, str(count))
+            self.inputs.append(input_path_name)
+            self.labels.append(label_path_name)
+            np.save(input_path_name, frames_clips[i])
+            np.save(label_path_name, bvps_clips[i])
+            count += 1
+        return count
