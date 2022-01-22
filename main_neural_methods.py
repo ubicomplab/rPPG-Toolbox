560fe731eea3437b31406d90813cf294a9d197a4
Xiaoyu Zhang <48305798+Xiaoyu-SZ@users.noreply.github.com>
2022-01-22 12:04:12 +0800

    Issue#5 dataloader (#8)
    * issue5-dataloader
    
    * update main_neural_methods.py

--- main_neural_methods.py
+++ main_neural_methods.py
@@ -1,40 +1,37 @@
-# TODO: Add descriptions for the file
+""" The main function for the rPPG deep learning pipeline.
 
+TODO: Adds detailed description for models and datasets supported.
+An end-to-end training pipleine for neural network methods.
 
-# Lint as: python3
-import numpy as np
-import torch
-from torch.utils.data import DataLoader
-from neural_methods.loss.NegPearsonLoss import Neg_Pearson
-from neural_methods.model.PhysNet import *
-from neural_methods.model.rPPGNet import *
-from dataset.data_loader.UBFC_loader import UBFC_loader
-from tensorboardX import SummaryWriter
+  Typical usage example:
+
+  python main.py --model_name physnet --data_dir "G:\\UBFC_data"
+"""
+
 import argparse
 import glob
-from neural_methods.trainer.physnet_trainer import physnet_trainer
-from neural_methods.trainer.trainer import trainer
-
 import os
 import time
+from torch.utils.data import DataLoader
+from dataset.data_loader.data_loader import data_loader
+from dataset.data_loader.UBFC_loader import UBFC_loader
+from tensorboardX import SummaryWriter
+from neural_methods.trainer.trainer import trainer
+from neural_methods.trainer.physnet_trainer import physnet_trainer
 
-
-def get_data():
-    bvp_files = glob.glob(args.data_dir + os.sep + "subject*/*.txt")
-    video_files = glob.glob(args.data_dir + os.sep + "subject*/*.avi")
-
+def get_UBFC_data(args):
+    """Returns directories for train sets, validation sets and test sets.
+    For the dataset structure, see dataset/dataloader/UBFC_dataloader.py """
+    data_dirs = glob.glob(args.data_dir + os.sep + "subject*")
     return {
-        "bvp": {
-            "train": bvp_files[:-2],
-            "valid": bvp_files[-2:-1],
-            "test": bvp_files[-1:]},
-        "video": {
-            "train": video_files[:-2],
-            "valid": video_files[-2:-1],
-            "test": video_files[-1:]}}
+        "train": data_dirs,
+        "valid": data_dirs,
+        "test": data_dirs
+    }
 
 
 def add_args(parser):
+    """Adds arguments for parser."""
     parser.add_argument('--model_name', default="physnet", type=str)
     parser.add_argument('--do_train', action='store_true')
     parser.add_argument(
@@ -45,46 +42,42 @@ def add_args(parser):
     parser.add_argument('--batch_size', default=4, type=int)
     parser.add_argument('--data_dir', default="G:\\UBFC_data",
                         type=str, help='The path of the data directory')
-    t = time.localtime()
-    parser.add_argument(
-        '--name', default="{0}-{1}".format(str(t.tm_hour), str(t.tm_min)), type=str)
+    current_time = time.localtime()
+    parser.add_argument('--name',
+                        default="{0}-{1}".format(str(current_time.tm_hour),
+                                                 str(current_time.tm_min)),
+                        type=str)
     parser.add_argument('--round_num_max', default=20, type=int)
     return parser
 
 
 def main(args, writer, data_loader):
+    """Trains the model."""
     trainer_name = eval('{0}_trainer'.format(args.model_name))
     trainner = trainer_name(args, writer)
     trainner.train(data_loader)
-    print("End")
 
 
 if __name__ == "__main__":
+    # parses arguments.
     parser = argparse.ArgumentParser()
     parser = add_args(parser)
-    args = parser.parse_args()
     parser = trainer.add_trainer_args(parser)
+    parser = data_loader.add_data_loader_args(parser)
     args = parser.parse_args()
     writer = SummaryWriter('runs/exp/' + args.name)
 
-    # load data
-    data_files = get_data()
+    # loads data
+    data_files = get_UBFC_data(args)
     train_data = UBFC_loader(
-        data_files["video"]["train"],
-        data_files["bvp"]["train"],
-        "train")
-    train_data.preprocessing()
+        data_dirs=data_files["train"],
+        cached_dir=args.cached_dir)
     valid_data = UBFC_loader(
-        data_files["video"]["valid"],
-        data_files["bvp"]["valid"],
-        "valid")
-    valid_data.preprocessing()
+        data_dirs=data_files["valid"],
+        cached_dir=args.cached_dir)
     test_data = UBFC_loader(
-        data_files["video"]["test"],
-        data_files["bvp"]["test"],
-        "test")
-    test_data.preprocessing()
-
+        data_dirs=data_files["test"],
+        cached_dir=args.cached_dir)
     dataloader = {
         "train": DataLoader(
             dataset=train_data,
