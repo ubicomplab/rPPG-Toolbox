5850a68cbca7c39e80c8bf4eeb78886648cef8bc
Xiaoyu Zhang <48305798+Xiaoyu-SZ@users.noreply.github.com>
2022-01-22 12:42:36 +0800

    add utils (#10)

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+""" Short functions for data-preprocessing and data-loading. """
+
+import numpy as np
+
+
+def next_power_of_2(x):
+    return 1 if x == 0 else 2**(x - 1).bit_length()
+
+
+def sample(a, len):
+    """Samples a sequence into specific length."""
+    return np.interp(
+        np.linspace(
+            1, a.shape[0], len), np.linspace(
+            1, a.shape[0], a.shape[0]), a)
