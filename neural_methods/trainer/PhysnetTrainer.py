18442e38cdfbd03c7601c992209c72688eba975b
Your Name <you@example.com>
2022-07-20 00:16:27 +0800

    support PhysNet

--- neural_methods/trainer/PhysnetTrainer.py
+++ neural_methods/trainer/PhysnetTrainer.py
@@ -1,4 +1,4 @@
-#TODO: Docstring
+# TODO: Docstring
 """A one line summary of the module or program, terminated by a period.
 
 Leave one blank line.  The rest of this docstring should contain an
@@ -20,6 +20,8 @@ import torch.optim as optim
 import numpy as np
 import os
 from tqdm import tqdm
+from metrics.metrics import calculate_metrics
+from collections import OrderedDict
 
 
 class PhysnetTrainer(BaseTrainer):
@@ -28,29 +30,32 @@ class PhysnetTrainer(BaseTrainer):
         """Inits parameters from args and the writer for TensorboardX."""
         super().__init__()
         self.device = torch.device(config.DEVICE)
-        logging.debug(self.device)
         self.model = PhysNet_padding_Encoder_Decoder_MAX(
             frames=config.MODEL.PHYSNET.FRAME_NUM).to(self.device)  # [3, T, 128,128]
         self.loss_model = Neg_Pearson()
         self.optimizer = optim.Adam(
             self.model.parameters(), lr=config.TRAIN.LR)
-        self.epochs = config.TRAIN.EPOCHS
+        self.max_epoch_num = config.TRAIN.EPOCHS
         self.model_dir = config.MODEL.MODEL_DIR
         self.model_file_name = config.TRAIN.MODEL_FILE_NAME
-        logging.debug(self.device)
+        self.batch_size = config.TRAIN.BATCH_SIZE
+        self.num_of_gpu = config.NUM_OF_GPU_TRAIN
+        self.base_len = self.num_of_gpu
+        self.config = config
+        self.best_epoch = 0
 
     def train(self, data_loader):
         """ TODO:Docstring"""
         if data_loader["train"] is None:
             assert ValueError("No data for train")
         min_valid_loss = 1
-        for round in range(self.epochs):
-            logging.debug(f"====training:ROUND{round}====")
+        for epoch in range(self.max_epoch_num):
+            print(f"====Training Epoch: {epoch}====")
+            running_loss = 0.0
             train_loss = []
             self.model.train()
             tbar = tqdm(data_loader["train"], ncols=80)
-
-            for i, batch in enumerate(tbar):
+            for idx, batch in enumerate(tbar):
                 tbar.set_description("Train epoch %s" % epoch)
                 rPPG, x_visual, x_visual3232, x_visual1616 = self.model(
                     Variable(batch[0]).to(torch.float32).to(self.device))
@@ -59,21 +64,28 @@ class PhysnetTrainer(BaseTrainer):
                 rPPG = (rPPG - torch.mean(rPPG)) / torch.std(rPPG)  # normalize
                 BVP_label = (BVP_label - torch.mean(BVP_label)) / \
                     torch.std(BVP_label)  # normalize
-                loss_ecg = self.loss_model(rPPG, BVP_label)
-                loss_ecg.backward()
-                train_loss.append(loss_ecg.item())
+                loss = self.loss_model(rPPG, BVP_label)
+                loss.backward()
+                running_loss += loss.item()
+                if idx % 100 == 99:  # print every 100 mini-batches
+                    print(
+                        f'[{epoch + 1}, {idx + 1:5d}] loss: {running_loss / 100:.3f}')
+                    running_loss = 0.0
+                train_loss.append(loss.item())
                 self.optimizer.step()
                 self.optimizer.zero_grad()
-                vbar.set_postfix(loss=loss_ecg.item())
-            train_loss = np.asarray(train_loss)
-            logging.debug(np.mean(train_loss))
-
+                tbar.set_postfix(loss=loss.item())
             valid_loss = self.valid(data_loader)
-            # saves the model according to the loss on valid sets.
-            if(valid_loss < min_valid_loss):
+            self.save_model(epoch)
+            print('validation loss: ', valid_loss)
+            if(valid_loss < min_valid_loss) or (valid_loss < 0):
                 min_valid_loss = valid_loss
-                logging.debug("update best model")
-                self.save_model()
+                self.best_epoch = epoch
+                print("Update best model! Best epoch: {}".format(self.best_epoch))
+                self.save_model(epoch)
+        print("best trained epoch:{}, min_val_loss:{}".format(
+            self.best_epoch, min_valid_loss))
+        return 0
 
     def valid(self, data_loader):
         """ Runs the model on valid sets."""
@@ -86,9 +98,9 @@ class PhysnetTrainer(BaseTrainer):
         with torch.no_grad():
             vbar = tqdm(data_loader["valid"], ncols=80)
 
-            for valid_i, valid_batch in enumerate(vbar):
+            for valid_idx, valid_batch in enumerate(vbar):
                 vbar.set_description("Validation")
-                #vbar.set_description("Valid %s" % valid_i)
+                # vbar.set_description("Valid %s" % valid_i)
                 BVP_label = Variable(valid_batch[1]).to(
                     torch.float32).to(self.device)
                 rPPG, x_visual, x_visual3232, x_visual1616 = self.model(
@@ -99,39 +111,52 @@ class PhysnetTrainer(BaseTrainer):
                 loss_ecg = self.loss_model(rPPG, BVP_label)
                 valid_loss.append(loss_ecg.item())
                 valid_step += 1
-                train_loss.append(loss.item())
                 vbar.set_postfix(loss=loss_ecg.item())
-            valid_loss = np.asarray(valid_loss.item())
-            print(np.mean(valid_loss))
+            valid_loss = np.asarray(valid_loss)
         return np.mean(valid_loss)
 
     def test(self, data_loader):
         """ Runs the model on test sets."""
         if data_loader["test"] is None:
             assert ValueError("No data for test")
-        logging.debug(" ====testing===")
-        test_step = 0
-        test_loss = []
+        config = self.config
+        print("===Testing===")
+        predictions = dict()
+        labels = dict()
+        if config.TRAIN_OR_TEST == "only_test":
+            self.model.load_state_dict(torch.load(config.INFERENCE.MODEL_PATH))
+            print("Testing uses pretrained model!")
+        else:
+            best_model_path = os.path.join(
+                self.model_dir, self.model_file_name + '_Epoch' + str(self.best_epoch) + '.pth')
+            print("Testing uses non-pretrained model!")
+            print(best_model_path)
+            self.model.load_state_dict(torch.load(best_model_path))
+        self.model = self.model.to(config.DEVICE)
         self.model.eval()
         with torch.no_grad():
-            for test_i, test_batch in enumerate(data_loader["test"]):
-                BVP_label = Variable(test_batch[1]).to(
-                    torch.float32).to(self.device)
-                rPPG, x_visual, x_visual3232, x_visual1616 = self.model(
-                    Variable(test_batch[0]).to(torch.float32).to(self.device))
-                rPPG = (rPPG - torch.mean(rPPG)) / torch.std(rPPG)  # normalize
-                BVP_label = (BVP_label - torch.mean(BVP_label)) / \
-                    torch.std(BVP_label)  # normalize
-                loss_ecg = self.loss_model(rPPG, BVP_label)
-                test_step += 1
-                logging.debug(loss_ecg.item())
-                test_loss.append(test_loss)
-        return np.mean(test_loss)
+            for _, test_batch in enumerate(data_loader['test']):
+                batch_size = test_batch[0].shape[0]
+                data, label = test_batch[0].to(
+                    config.DEVICE), test_batch[1].to(config.DEVICE)
+                prediction, _, _, _ = self.model(data)
+                for idx in range(batch_size):
+                    subj_index = test_batch[2][idx]
+                    sort_index = int(test_batch[3][idx])
+                    if subj_index not in predictions.keys():
+                        predictions[subj_index] = dict()
+                        labels[subj_index] = dict()
+                    predictions[subj_index][sort_index] = prediction
+                    labels[subj_index][sort_index] = label
+        calculate_metrics(predictions, labels, config)
+        return 0
 
-    def save_model(self):
-        if(not os.path.exists(self.model_dir)):
+    def save_model(self, index):
+        if not os.path.exists(self.model_dir):
             os.makedirs(self.model_dir)
-        torch.save(self.model.state_dict(), os.path.join(
-            self.model_dir, self.model_file_name))
+        model_path = os.path.join(
+            self.model_dir, self.model_file_name + '_Epoch' + str(index) + '.pth')
+        torch.save(self.model.state_dict(), model_path)
+        print('Saved Model Path: ', model_path)
 
     # def load_model(self):
