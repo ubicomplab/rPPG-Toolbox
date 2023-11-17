86688913da247745e16a7bbef7e5f9363f6cab9b
Jack Tang <73820234+McJackTang@users.noreply.github.com>
2023-11-18 01:10:37 +0800

    Update PhysFormerTrainer.py
    Add saving output for further analysis

--- neural_methods/trainer/PhysFormerTrainer.py
+++ neural_methods/trainer/PhysFormerTrainer.py
@@ -250,6 +250,8 @@ class PhysFormerTrainer(BaseTrainer):
 
         print('')
         calculate_metrics(predictions, labels, self.config)
+        if self.config.TEST.OUTPUT_SAVE_DIR: # saving test outputs
+            self.save_test_outputs(predictions, labels, self.config)
 
     def save_model(self, index):
         if not os.path.exists(self.model_dir):
