3c1549822618dca2a889855b21b847921dd3cbf9
Gauthier <mail@gauthierth.fr>
2024-10-29 14:30:43 +0100

    Remove unnecessary changes

--- main.py
+++ main.py
@@ -82,7 +82,6 @@ def train_and_test(config, data_loader_dict):
     else:
         raise ValueError('Your Model is Not Supported  Yet!')
     model_trainer.train(data_loader_dict)
-    model_trainer.valid(data_loader_dict)
     model_trainer.test(data_loader_dict)
 
 
@@ -197,8 +196,6 @@ if __name__ == "__main__":
             valid_loader = data_loader.MMPDLoader.MMPDLoader
         elif config.VALID.DATA.DATASET == "BP4DPlus":
             valid_loader = data_loader.BP4DPlusLoader.BP4DPlusLoader
-        elif config.VALID.DATA.DATASET == "COHFACE":
-            valid_loader = data_loader.COHFACELoader.COHFACELoader
         elif config.VALID.DATA.DATASET == "BP4DPlusBigSmall":
             valid_loader = data_loader.BP4DPlusBigSmallLoader.BP4DPlusBigSmallLoader
         elif config.VALID.DATA.DATASET == "UBFC-PHYS":
@@ -214,7 +211,6 @@ if __name__ == "__main__":
         # Create and initialize the valid dataloader given the correct toolbox mode,
         # a supported dataset name, and a valid dataset path
         if (config.VALID.DATA.DATASET and config.VALID.DATA.DATA_PATH and not config.TEST.USE_LAST_EPOCH):
-        # if (config.VALID.DATA.DATASET and config.VALID.DATA.DATA_PATH):
             valid_data = valid_loader(
                 name="valid",
                 data_path=config.VALID.DATA.DATA_PATH,
