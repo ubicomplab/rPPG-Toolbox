Fixing the RMSE standard_error calculation in unsupervised_predictor.py (#356)
* Fixing the RMSE standard_error calculation in unsupervised_predictor.py

The square root was missing in standard_error calculation.

* Update unsupervised_predictor.py

added comments to the RMSE standard error calculation inside unsupervised predictor.py

unsupervised_methods/unsupervised_predictor.py
@@ -97,8 +97,12 @@ def unsupervised_predict(config, data_loader, method_name):
                 standard_error = np.std(np.abs(predict_hr_peak_all - gt_hr_peak_all)) / np.sqrt(num_test_samples)
                 print("Peak MAE (Peak Label): {0} +/- {1}".format(MAE_PEAK, standard_error))
             elif metric == "RMSE":
-                RMSE_PEAK = np.sqrt(np.mean(np.square(predict_hr_peak_all - gt_hr_peak_all)))
-                standard_error = np.std(np.square(predict_hr_peak_all - gt_hr_peak_all)) / np.sqrt(num_test_samples)
+                # Calculate the squared errors, then RMSE, in order to allow
+                # for a more robust and intuitive standard error that won't
+                # be influenced by abnormal distributions of errors.
+                squared_errors = np.square(predict_hr_peak_all - gt_hr_peak_all)
+                RMSE_PEAK = np.sqrt(np.mean(squared_errors))
+                standard_error = np.sqrt(np.std(squared_errors) / np.sqrt(num_test_samples))
                 print("PEAK RMSE (Peak Label): {0} +/- {1}".format(RMSE_PEAK, standard_error))
             elif metric == "MAPE":
                 MAPE_PEAK = np.mean(np.abs((predict_hr_peak_all - gt_hr_peak_all) / gt_hr_peak_all)) * 100
@@ -145,8 +149,12 @@ def unsupervised_predict(config, data_loader, method_name):
                 standard_error = np.std(np.abs(predict_hr_fft_all - gt_hr_fft_all)) / np.sqrt(num_test_samples)
                 print("FFT MAE (FFT Label): {0} +/- {1}".format(MAE_FFT, standard_error))
             elif metric == "RMSE":
-                RMSE_FFT = np.sqrt(np.mean(np.square(predict_hr_fft_all - gt_hr_fft_all)))
-                standard_error = np.std(np.square(predict_hr_fft_all - gt_hr_fft_all)) / np.sqrt(num_test_samples)
+                # Calculate the squared errors, then RMSE, in order to allow
+                # for a more robust and intuitive standard error that won't
+                # be influenced by abnormal distributions of errors.
+                squared_errors = np.square(predict_hr_fft_all - gt_hr_fft_all)
+                RMSE_FFT = np.sqrt(np.mean(squared_errors))
+                standard_error = np.sqrt(np.std(squared_errors) / np.sqrt(num_test_samples))
                 print("FFT RMSE (FFT Label): {0} +/- {1}".format(RMSE_FFT, standard_error))
             elif metric == "MAPE":
                 MAPE_FFT = np.mean(np.abs((predict_hr_fft_all - gt_hr_fft_all) / gt_hr_fft_all)) * 100